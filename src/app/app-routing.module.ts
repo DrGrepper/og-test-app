b3fbfafc86eb8e2c44913cd38abcc3e4b6415398
jdobson <jdobson.dev@gmail.com>
2023-09-22 16:21:36 -0400

    add fallback route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { OgImagesComponent } from './og-images/og-images.component';
 import { ResourcesComponent } from './resources/resources.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -11,7 +12,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
