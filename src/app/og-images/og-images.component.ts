a5bc71d29f6b42faa9b2d931b4066b0f536a4864
jdobson <jdobson.dev@gmail.com>
2023-09-22 21:40:22 -0400

    add meta tags to og-images route

--- src/app/og-images/og-images.component.ts
+++ src/app/og-images/og-images.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-og-images',
   templateUrl: './og-images.component.html',
   styleUrls: ['./og-images.component.scss']
 })
-export class OgImagesComponent {
-
+export class OgImagesComponent implements OnInit{
+  constructor(private meta: Meta){}
+  ngOnInit(): void {
+    this.meta.updateTag({property: 'og:title', content: 'Open Graph Images Documentation'});
+    this.meta.updateTag({property: 'og:description', content: 'Here is some documentation on OG Images'});
+    //this.meta.updateTag({property: 'og:image', content: 'YOUR_URL'});
+    this.meta.updateTag({property: 'og:url', content: 'https://metametameta.net/og-images'});
+  }
 }
